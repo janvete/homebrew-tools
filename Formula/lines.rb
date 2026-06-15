c493370272d25c53a4464823b2720f0bf3bde447
SW <janvete@gmail.com>
2026-06-15 11:11:19 +0200

    Update lines to v0.1.2 with configurable terminal

--- Formula/lines.rb
+++ Formula/lines.rb
@@ -1,8 +1,8 @@
 class Lines < Formula
   desc "TUI for running frequently used commands from markdown files"
   homepage "https://github.com/janvete/lines"
-  url "https://github.com/janvete/lines/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "5314c14515f0ef8834bc0dbd72a2ec1f5d5f5e14c9c7de6d4d5e69981b896b12"
+  url "https://github.com/janvete/lines/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "e8bd86ac4c99dd08bc0944af90b7c6c1db136f4cd7698d52a387f34d4de41eb4"
   license "MIT"
   head "https://github.com/janvete/lines.git", branch: "main"
 
