e690711eda5005bcc71fae69ffaf56ce41bd7f2a
SW <janvete@gmail.com>
2026-06-15 13:40:55 +0200

    Update lines to v0.1.7 — add history.log

--- Formula/lines.rb
+++ Formula/lines.rb
@@ -1,8 +1,8 @@
 class Lines < Formula
   desc "TUI for running frequently used commands from markdown files"
   homepage "https://github.com/janvete/lines"
-  url "https://github.com/janvete/lines/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "182a9ef9e301c1d2169bb6634816cc542d12b8de92ca52fe3fb063cf215e9dc3"
+  url "https://github.com/janvete/lines/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "c59edb20ae8153c041664ec47ca94c1e01640a7ee94e724e584709435c539de2"
   license "MIT"
   head "https://github.com/janvete/lines.git", branch: "main"
 
