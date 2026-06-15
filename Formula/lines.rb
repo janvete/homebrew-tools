7b5f5995cc06064bf48ca4bb99ffc1391def00fb
SW <janvete@gmail.com>
2026-06-15 13:21:16 +0200

    Update lines to v0.1.6 — run commands in current terminal by default

--- Formula/lines.rb
+++ Formula/lines.rb
@@ -1,8 +1,8 @@
 class Lines < Formula
   desc "TUI for running frequently used commands from markdown files"
   homepage "https://github.com/janvete/lines"
-  url "https://github.com/janvete/lines/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "2aee5b44f40cb1848376ca83642439a609020bebd4439dadcb19ed3e29c8adec"
+  url "https://github.com/janvete/lines/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "182a9ef9e301c1d2169bb6634816cc542d12b8de92ca52fe3fb063cf215e9dc3"
   license "MIT"
   head "https://github.com/janvete/lines.git", branch: "main"
 
